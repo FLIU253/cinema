fc40037e60a139127747b28eaf5f61aa18f552b1
Franklin Liu <FLIU253@uwo.ca>
2021-02-14 13:28:09 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import './App.scss';
 import {Provider} from 'react-redux';
 import store from './redux/store';
-
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <Provider store = {store} >
-    <div className="app">
-      Redux
-    </div>
+     <div className="app">
+        <Header/>
+        <h1>Redux</h1>
+      </div>
     </Provider>
   );
 }

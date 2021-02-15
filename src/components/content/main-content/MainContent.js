86477b3e132d648901dd28112d7ab93c9ccc7aef
Franklin Liu <FLIU253@uwo.ca>
2021-02-15 09:53:53 -0500

    added paginate feature

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './MainContent.scss'
 import '../slide-show/Slideshow';
 import Slideshow from '../slide-show/Slideshow';
+import Paginate from '../paginate/Paginate';
 
 const MainContent = () => {
     const images = [
@@ -15,12 +16,22 @@ const MainContent = () => {
             url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU'
     }];
 
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const paginate = (type) => {
+        if(type === "prev" && currentPage > 1){
+            setCurrentPage((prev) => prev -1);
+        }else {
+            setCurrentPage((prev) => prev + 1);
+        }
+    }
+
     return (
         <div className="main-content">
             <Slideshow images = {images} auto={true} showArrows = {true}/>
             <div className="grid-movie-title">
                 <div className="movieType">Now Playing</div>
-                <div className="paginate">Paginate</div>
+                <div className="paginate"><Paginate paginate = {paginate} currentPage = {currentPage} totalPages = {10} /></div>
             </div>
             {/* Display grid component */}
         </div>

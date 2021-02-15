da28b51cf1fb5a4e8317c2d9d978f753a7f2e7be
Franklin Liu <FLIU253@uwo.ca>
2021-02-15 11:07:36 -0500

    Added grid system and rating system

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -3,18 +3,39 @@ import './MainContent.scss'
 import '../slide-show/Slideshow';
 import Slideshow from '../slide-show/Slideshow';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
     const images = [
         {
-            url: 'https://6.viki.io/image/4db090be7c3b4285ba59cd678100398a.jpeg?s=900x600&e=t'
+            url: 'https://6.viki.io/image/4db090be7c3b4285ba59cd678100398a.jpeg?s=900x600&e=t',
+            rating: 7.5
         }, 
         {
-            url: 'https://www.thefamouspeople.com/profiles/images/kim-tae-ri-2.jpg'
+            url: 'https://www.thefamouspeople.com/profiles/images/kim-tae-ri-2.jpg',
+            rating: 7
         },
         {
-            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU'
-    }];
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU',
+            rating: 8.5
+        },
+        {
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU',
+            rating: 1.5
+        },
+        {
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU',
+            rating: 6.6
+        },
+        {
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU',
+            rating: 7.5
+        },
+        {
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxq5ebU3v4oOzEwmM7V15EUArWrW5Cq9MEDw&usqp=CAU',
+            rating: 7.5
+        }
+];
 
     const [currentPage, setCurrentPage] = useState(1);
 
@@ -34,6 +55,7 @@ const MainContent = () => {
                 <div className="paginate"><Paginate paginate = {paginate} currentPage = {currentPage} totalPages = {10} /></div>
             </div>
             {/* Display grid component */}
+            <Grid images = {images} />
         </div>
     )
 }
